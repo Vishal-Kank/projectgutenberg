08929dcad7b583fbd1428daaed715789232b2f80
Vishal-Kank <kankvishalkank@gmail.com>
2019-11-14 14:22:06 +0530

    Updates

--- precache-manifest.54935710c2d55e5b687c871358c8897e.js
+++ precache-manifest.54935710c2d55e5b687c871358c8897e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c09fb4c7dd634c43e5d06a92874c71e",
+    "revision": "1f3b5b63e1ad5b2fe7f1ce60052ae0f3",
     "url": "/projectgutenberg/index.html"
   },
   {
-    "revision": "7853c426d331365b610f",
+    "revision": "d0dd8d8faf623ea9f06d",
     "url": "/projectgutenberg/static/css/main.7c164586.chunk.css"
   },
   {
-    "revision": "9c8f303bd3c77f0745eb",
-    "url": "/projectgutenberg/static/js/2.0055377f.chunk.js"
+    "revision": "a531e79baff50e8e133d",
+    "url": "/projectgutenberg/static/js/2.aab49a5f.chunk.js"
   },
   {
-    "revision": "7853c426d331365b610f",
-    "url": "/projectgutenberg/static/js/main.a5c43414.chunk.js"
+    "revision": "d0dd8d8faf623ea9f06d",
+    "url": "/projectgutenberg/static/js/main.d1570045.chunk.js"
   },
   {
     "revision": "0f4f825d86c56a4a8df7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projectgutenberg/precache-manifest.83931415640286387d3bb0465ce32c84.js"
+  "/projectgutenberg/precache-manifest.54935710c2d55e5b687c871358c8897e.js"
 );
 
 self.addEventListener('message', (event) => {
